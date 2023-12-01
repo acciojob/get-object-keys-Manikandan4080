b49d6d7139a1485dbfc1cb5b7a106a8e1fbc2576
Manikandan4080 <113871926+Manikandan4080@users.noreply.github.com>
2023-12-01 19:22:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let student = {name:"name"};
+Object.prototype.getKeys= function(){
+	let ans=[];
+	for(let i in this){
+	if (this.hasOwnProperty(i)) {
+		ans.push(i);
+	}
+}
+	return ans;
+}
